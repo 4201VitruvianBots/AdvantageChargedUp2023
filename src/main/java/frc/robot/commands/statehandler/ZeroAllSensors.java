952c3bd7530c2eb19ed3799db48f69e8bd15df7a
Gavin P <gavinjpeeler@gmail.com>
2023-10-20 17:39:01 -0700

    Fixed errors and revived Elevator.java

--- src/main/java/frc/robot/commands/statehandler/ZeroAllSensors.java
+++ src/main/java/frc/robot/commands/statehandler/ZeroAllSensors.java
@@ -5,11 +5,9 @@
 package frc.robot.commands.statehandler;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.SwerveDrive;
-import frc.robot.subsystems.Wrist;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.SwerveDrive.SwerveDrive;
+import frc.robot.subsystems.Wrist;
 
 public class ZeroAllSensors extends CommandBase {
   /** Creates a new ZeroAll. */
--- src/main/java/frc/robot/subsystems/SwerveDrive/ModuleIOInputsAutoLogged.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive/ModuleIOInputsAutoLogged.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.drive;
+package frc.robot.subsystems.SwerveDrive;
 
 import java.lang.Cloneable;
 import java.lang.Override;
